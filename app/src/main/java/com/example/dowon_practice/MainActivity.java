a93f4e12b5585ac4bd0afdd7d28278345d18c09f
onekorea37 <onekorea37@gmail.com>
2023-04-12 14:59:28 -0400

    Asus commit

--- app/src/main/java/com/example/dowon_practice/MainActivity.java
+++ app/src/main/java/com/example/dowon_practice/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.dowon_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startSignUpActivity();
+        }
+
+        findViewById(R.id.logoutButton).setOnClickListener(onClickListener);
+
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.logoutButton:
+                    FirebaseAuth.getInstance().signOut();
+                    startSignUpActivity();
+                    break;
+            }
+
+        }
+    };
+
+    private void startSignUpActivity() {
+        Intent intent = new Intent(this,SignUpActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/dowon_practice/SignUpActivity.java
+++ app/src/main/java/com/example/dowon_practice/SignUpActivity.java
@@ -1,10 +1,11 @@
 package com.example.dowon_practice;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,53 +28,73 @@ public class SignUpActivity extends AppCompatActivity {
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
-        findViewById(R.id.signUpButton).setOnClickListener(onClickListener);
+        findViewById(R.id.loginButton).setOnClickListener(onClickListener);
+        findViewById(R.id.gotoLoginButton).setOnClickListener(onClickListener);
 
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-   //        reload();
-        }
+    public void onBackPressed() {
+        super.onBackPressed();
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
     }
 
-    View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
+    View.OnClickListener onClickListener = (view) -> {
             switch (view.getId()) {
-                case R.id.signUpButton:
+                case R.id.loginButton:
                     signUp();
                     break;
+                case R.id.gotoLoginButton:
+                    startLoginActivity();
+                    break;
             }
-        }
     };
 
     private void signUp() {
 
         String email = ((EditText)findViewById(R.id.emailEditText)).getText().toString();
         String password = ((EditText)findViewById(R.id.passwordEditText)).getText().toString();
+        String passwordCheck =  ((EditText)findViewById(R.id.passwordCheckEditText)).getText().toString();
+
+        if(email.length() > 0 && password.length() > 0 && passwordCheck.length() > 0) {
+            if(password.equals(passwordCheck)) {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    startToast("회원가입에 성공했습니다.");
+                                    // UI
+                                } else {
+                                    if(task.getException() !=null) {
+                                        startToast(task.getException().toString());
+                                    }
+                                }
+                            }
+                        });
+            } else {
+                startToast("비밀번호가 일치하지 않습니다.");
+            }
+        } else {
+            startToast("이메일 또는 비밀번호를 입력해주세요.");
+        }
 
 
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            // UI
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            // UI
-                        }
-                    }
-                });
+
+    }
+
+    private void startToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void startLoginActivity() {
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
     }
 
 }
